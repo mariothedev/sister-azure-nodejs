8c903734ea8fcce5a2a1ada968fc9828efb194ee
mariothedev <mariothedev@gmail.com>
2023-01-11 13:07:51 +0100

    Add outbound

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const axios = require("axios");
 const app = require("express")();
 require("dotenv").config();
 
 app
-  .get("/", (_, res) => {
-    res.send("hello world 2.0");
+  .get("/", async (_, res) => {
+    // Make a request for a user with a given ID
+    axios
+      .get("https://api.nationalize.io?name=mario")
+      .then(function (response) {
+        // handle success
+        res.send(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        // handle error
+        res.send("Problem");
+      });
   })
   .listen(process.env.PORT || 3000);

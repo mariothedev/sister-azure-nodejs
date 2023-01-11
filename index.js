5cc459af1b26fc0b8fd70f9b19a20dff92546838
mariothedev <mariothedev@gmail.com>
2023-01-11 13:27:43 +0100

    Add webhook

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const axios = require("axios");
 const app = require("express")();
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 app
+  .use(bodyParser.json())
   .get("/", async (_, res) => {
     // Make a request for a user with a given ID
     axios
@@ -16,4 +18,22 @@ app
         res.send("Problem");
       });
   })
+  .post("/webhook", async (_, res) => {
+    axios
+      .post(
+        "https://hooks.slack.com/services/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",
+        {
+          text: "testing: please ignore",
+        }
+      )
+      .then(function (response) {
+        // handle success
+        res.send(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        // handle error
+        res.send("Problem");
+      });
+  })
+
   .listen(process.env.PORT || 3000);
